621bbc86d8102489812c982daf17f13737f0e4ca
Stevie Peck <peckstevie204@gmail.com>
2024-08-05 02:41:03 -0400

    Add resize button

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function initialize() {
+  const resize = document.getElementById("resize");
+  resize.addEventListener("click", resizeGrid);
   const body = document.querySelector("body");
-  body.appendChild(createGrid(16));
+  body.insertBefore(createGrid(16), resize);
 }
 
 function createGrid(size) {
@@ -25,10 +27,27 @@ function fillWithCopies(container, element, amount) {
   return container;
 }
 
+function deleteGrid() {
+  document.getElementById("grid").remove();
+}
+
 function changeColor(event) {
   if (event.target.classList.contains("square")) {
     event.target.classList.toggle("filled");
   }
+}
+
+function resizeGrid(event) {
+  const body = document.querySelector("body");
+  let size = +prompt("Size of new grid (4-100 squares per side)");
+  if (!Number.isInteger(size) || size < 4) {
+    size = 4;
+  } else if (size > 100) {
+    size = 100;
+  }
+
+  deleteGrid();
+  body.insertBefore(createGrid(size), event.target);
 }
 
 initialize();
