7f33df2f6ac66bbd27abc20a791bb4bb5fe25b98
Stevie Peck <peckstevie204@gmail.com>
2024-08-05 01:35:09 -0400

    Create initial grid

---
+++ script.js
@@ -0,0 +1,27 @@
+function initialize() {
+  const body = document.querySelector("body");
+  body.appendChild(createGrid(16))
+}
+
+function createGrid(size) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+  let row = document.createElement("div");
+  row.classList.add("row");
+  let grid = document.createElement("div");
+  grid.id = "grid";
+  
+  row = fillWithCopies(row, square, size);
+  grid = fillWithCopies(grid, row, size);
+  return grid;
+}
+
+function fillWithCopies(container, element, amount) {
+  for (let i = 0; i < amount; i++) {
+    container.appendChild(element.cloneNode(true));
+  }
+
+  return container;
+}
+
+initialize();
